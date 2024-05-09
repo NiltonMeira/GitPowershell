4554773abdc072b46b300dede3344f1b50c6aaf5
Janeckiisa <janeckiisabelle@gmail.com>
2024-05-09 14:45:32 -0300

    Finalizando

--- nisaInit.ps1
+++ nisaInit.ps1
@@ -6,6 +6,7 @@ function nisa {
 
     $currentDirectory = (Get-Location).Path
     $destinationDirectory = "$currentDirectory/nisa"
+    $arquivo = Get-ChildItem -Path $currentDirectory
     
     $i = 0
 
@@ -17,27 +18,49 @@ function nisa {
             Write-Host "Directory created."
         }
 
+        add {   
+
+            if ($name -eq ".") {
+                foreach ($file in $arquivo) {
+                    if ($file -notmatch "nisa") {
+                        Add-Content -Path "$destinationDirectory\add.txt" -Value $file
+                    }
+                }
+            }
+
+            else {
+                foreach ($file in $arquivo) {
+                    if ($name -eq $file) {
+                        Add-Content -Path "$destinationDirectory\add.txt" -Value $file
+                        Write-Host "the file $file was added"
+                        break
+                    }
+                }
+                Write-Host "This file doesn't exists"
+            }
+        }
+
         commit {
 
-            $arquivo = Get-ChildItem -Path $currentDirectory
-            [string[]]$addContent = get-content -Path "$currentDirectory/add.txt"
+            [string[]]$addContent = get-content -Path "$destinationDirectory/add.txt"
             Write-Host $addContent
-
-
+    
+    
             for ($i = 0; $i -lt $addContent.Count; $i++) {
                 for ($j = 0; $j -lt $arquivo.Count; $j++) {
                     if ($addContent[$i] -eq $arquivo[$j].Name) {
                         if (-not(Test-Path -Path "$destinationDirectory/$name")) {
                             New-item -Path "$destinationDirectory/$name" -ItemType Directory
                         }
-                        Copy-Item $arquivo[$j] -Destination "$destinationDirectory/$name" 
+                        Copy-Item $arquivo[$j] -Destination "$destinationDirectory/$name" -Recurse
                     }
                 }
             }
+            Set-Content -Path "$destinationDirectory/add.txt" -Value ""   
         }
-
+    
         Default {
             Write-Output "Invalid command."
         }
-    }
+    }   
 }
